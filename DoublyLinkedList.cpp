14596340203381f5e3657699c96bd30b81a4b9f8
fellowpandit <51901277+fellowpandit@users.noreply.github.com>
2023-02-09 12:16:27 +0530

    Update DoublyLinkedList.cpp

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -5,7 +5,6 @@ using namespace std;
  This Program works on the concept that, Doubly Linked list is two Singly Linked list with a single node.
  There is a single node with two fronts named as "head" and "tail".
  Basically Singly nodes inbuilt which can traverse from both sides.
-
  *****"Tail" can also be regarded as "First"*****
  */
 
@@ -25,24 +24,20 @@ public:
 class DoublyLinkedList{
 private:
     Node* head;
-    Node* tail;
 public:
     void insertAtHead(int data){
         Node* newNode = new Node(data);
         newNode->next = head;
-        cout<<head<<endl;
         if(head!=NULL) head->prev = newNode;
         head = newNode;
-        cout<<"YES";
-
-        if(newNode->next == nullptr){       //If there were no nodes previously then head = newNode = tailNode
-            tail = newNode;
-        }
-        cout<<"Inserted!!"<<endl;
     }
 
     void insertAtFirst(int data){
         Node* newNode = new Node(data);
+        Node* tail = head;
+        while(tail->next){
+            tail = tail->next;
+        }
         tail->next = newNode;
         newNode->prev = tail;
         tail = newNode;
@@ -55,12 +50,20 @@ public:
         Node* newNode = new Node(data);     //The new Node
         Node* temp = head;                  //Node1 in front of which new Node to be added
 
+        if(Pos == 0){                       //To Put the element at Head [Mechanism of adding node to "head" is different.]
+            insertAtHead(data);
+            return;
+        }
         int i = 0;
-        while((i<Pos-1)&&temp->next){
+        while(i<Pos-1){
+            if(!temp->next->next){          //Check insertAtFirst() method to understand this more.
+                insertAtFirst(data);        //To Put the element at Tail [Mechanism of adding node to "tail" is different.]
+                return;
+            }
+
             temp = temp->next;
             i++;
         }
-        cout<<"YES";
 
         Node* temp2 = temp->next;           //Node2 in whose previous new Node to be added
         temp->next = newNode;               //Previous Node is now pointing forward to the new Node
@@ -75,7 +78,11 @@ public:
         head->prev = nullptr;
     }
 
-    void deleteAtTail(){
+    void deleteAtFirst(){
+        Node* tail = head;
+        while(tail->next){
+            tail = tail->next;
+        }
         Node* temp = tail->prev;
         tail = temp;
         tail->next = nullptr;
@@ -83,8 +90,18 @@ public:
 
     void deleteAtPos(int Pos){
         Node* temp = head;                  //Previous Node from the "Node to be deleted"
+
+        if(Pos == 0){                       //To Remove the element at Head [Remember the mechanism of deleting node at "head" is different.]
+            deleteAtHead();
+            return;
+        }
+
         int i = 0;
-        while((i<Pos-1)&&temp->next){
+        while(i<Pos-1){
+            if(!temp->next->next->next){    //Check deleteAtFirst() method to understand this more
+                deleteAtFirst();            //To Remove the element at Tail [Mechanism of removing node at "tail" is different.]
+                return;
+            }
             temp = temp->next;
             i++;
         }
@@ -98,7 +115,7 @@ public:
 
     void printFromHead(){
         Node* temp = head;
-        while(temp->next){
+        while(temp){
             cout<<temp->data<<"\t->";
             temp = temp->next;
         }
@@ -106,74 +123,78 @@ public:
     }
 
     void printFromFirst(){
-        Node* temp = tail;
-        while(temp->prev){
-            cout<<temp->data<<"\t<-";
-            temp = temp->prev;
+        Node* tail = head;
+        while(tail->next){
+            tail = tail->next;
         }
-        cout<<"NULL"<<endl;
+        cout<<"NULL";
+        while(tail){
+            cout<<"\t->"<<tail->data;
+            tail = tail->prev;
+        }
+        cout<<endl;
     }
 };
 
 int main() {
     DoublyLinkedList list;
-    list.insertAtHead(55);
-//    int op = 1;
-//    cout<<"\n****MENU****\n\n"<<
-//    "1.Insert at Head\n"<<
-//    "2.Insert at First\n"<<
-//    "3.Insert at Position\n"<<
-//    "4.Delete at Head\n"<<
-//    "5.Delete at First\n"<<
-//    "6.Delete at Position\n"<<
-//    "7.Display from Head\n"<<
-//    "8.Display from First\n"<<
-//    "0.Exit\n"<<endl;
-//
-//    while(op){
-//        cout<<"Enter Option: ";
-//        cin>>op;
-//        int data,pos;
-//        switch(op){
-//            case 0:
-//                break;
-//            case 1:
-//                cout<<"Enter Data: ";
-//                cin>>data;
-//                list.insertAtHead(data);
-//                break;
-//            case 2:
-//                cout<<"Enter Data: ";
-//                cin>>data;
-//                list.insertAtFirst(data);
-//                break;
-//            case 3:
-//                cout<<"Enter Data: ";
-//                cin>>data;
-//                cout<<"\nEnter Position: ";
-//                cin>>pos;
-//                list.insertAtPos(data,pos);
-//                break;
-//            case 4:
-//                list.deleteAtHead();
-//                break;
-//            case 5:
-//                list.deleteAtTail();
-//                break;
-//            case 6:
-//                cout<<"\nEnter Position: ";
-//                cin>>pos;
-//                list.deleteAtPos(pos);
-//                break;
-//            case 7:
-//                list.printFromHead();
-//                break;
-//            case 8:
-//                list.printFromFirst();
-//                break;
-//            default:
-//                cout<<"Invalid Option!"<<endl;
-//        }
-//    }
+
+    int op = 1;
+    cout<<"\n****MENU****\n\n"<<
+    "1.Insert at Head\n"<<
+    "2.Insert at First\n"<<
+    "3.Insert at Position\n"<<
+    "4.Delete at Head\n"<<
+    "5.Delete at First\n"<<
+    "6.Delete at Position\n"<<
+    "7.Display from Head\n"<<
+    "8.Display from First\n"<<
+    "0.Exit\n"<<endl;
+
+    while(op){
+        cout<<"Enter Option: ";
+        cin>>op;
+        int data,pos;
+        switch(op){
+            case 0:
+                break;
+            case 1:
+                cout<<"Enter Data: ";
+                cin>>data;
+                list.insertAtHead(data);
+                break;
+            case 2:
+                cout<<"Enter Data: ";
+                cin>>data;
+                list.insertAtFirst(data);
+                break;
+            case 3:
+                cout<<"Enter Data: ";
+                cin>>data;
+                cout<<"\nEnter Position[0-n]: ";
+                cin>>pos;
+                list.insertAtPos(data,pos);
+                break;
+            case 4:
+                list.deleteAtHead();
+                break;
+            case 5:
+                list.deleteAtFirst();
+                break;
+            case 6:
+                cout<<"\nEnter Position[0-n]: ";
+                cin>>pos;
+                list.deleteAtPos(pos);
+                break;
+            case 7:
+                list.printFromHead();
+                break;
+            case 8:
+                list.printFromFirst();
+                break;
+            default:
+                cout<<"Invalid Option!"<<endl;
+        }
+    }
     return 0;
 }
